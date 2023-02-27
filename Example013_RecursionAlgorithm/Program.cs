4ad6023e9a841cc9135c6dd128f87b4edbb2a5ce
Yurii <iyureg@gmail.com>
2023-02-27 15:31:12 +0300

    числа Фибоначчи через рекурсию

--- Example013_RecursionAlgorithm/Program.cs
+++ Example013_RecursionAlgorithm/Program.cs
@@ -89,10 +89,31 @@ double Factorial(int n){
 }
 
 void PrintFactorials(int n){
+    // 0! = 1
+    // 1! = 1
     for (int i = 1; i <= n; i++)
     {
         Console.WriteLine($"{i}! = {Factorial(i)}");
     }
 
 }
-PrintFactorials(40);
+//PrintFactorials(40);
+
+// ------------- Fibonacci --------------
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n - 1) + f(n - 2)
+
+double Fibonacci(int n){
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+void PrintFibonacci(int n){
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine($"F({i}) = {Fibonacci(i)}");
+    }
+}
+PrintFibonacci(40); // 45, не больше!!!
