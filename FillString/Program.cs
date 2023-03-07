5628ee1e3c75cacd9eea11cfa8fb14f283b18546
Yurii <iyureg@gmail.com>
2023-03-07 14:04:31 +0300

    сборка строки через for и рекурсию

---
+++ FillString/Program.cs
@@ -0,0 +1,22 @@
+﻿// заполнить строку числами от a до b, a <= b
+
+// итеративный подход
+string NumbersFor(int a, int b){
+    string result = String.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+
+// рекурсия
+
+string NumbersRec(int a, int b){
+    if(a<= b) return $"{a} " + NumbersRec(a + 1, b);
+    else return String.Empty;
+}
+Console.Clear();
+Console.WriteLine($"{NumbersFor(1, 10)}");
+Console.WriteLine($"{NumbersRec(1, 10)}");
